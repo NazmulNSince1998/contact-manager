1abb02b7b300f29c23e8c2ab77d1f4278c1f4c37
Nazmul Hossain <nazmul.pro.dev@gmail.com>
2023-06-12 17:55:11 +0600

    tired of commiting

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from "react-dom";
+import { HashRouter } from "react-router-dom";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+ReactDOM.render(
+  <HashRouter>
     <App />
-  </React.StrictMode>,
-)
+  </HashRouter>,
+  document.getElementById("root")
+);
+
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './App.jsx'
+// import './index.css'
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
